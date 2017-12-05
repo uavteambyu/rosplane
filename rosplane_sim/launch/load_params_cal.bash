aebb52c3bdd5e379c6d5fd4e91782e0d0e3e1e07
uavteambyu <uavteambyu@gmail.com>
2017-12-04 17:16:42 -0700

    moved bash script elsewhere

--- rosplane_sim/launch/load_params_cal.bash
+++
@@ -1,6 +0,0 @@
-set -x
-
-rosservice call /fixedwing/param_load_from_file ~/rosflight_ws/param.yml
-rosservice call /fixedwing/calibrate_imu
-rosservice call /fixedwing/calibrate_baro
-rosservice call /fixedwing/calibrate_airspeed
